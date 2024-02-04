dbc1eac0e2e6f34913f4958f06ae2dea17c4b6f4
AURUM-MAINFRAME\chris <chrismartinezdeveloper@gmail.com>
2024-02-04 12:17:47 -0500

    (Task) Passing jobListing data to the index.html template; (Task) Formatting data for each job listing in index.html

--- app_jobBoard/views.py
+++ app_jobBoard/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from .models import JobListing
 
 # Create your views here.
 
 def HomePage(request):
-    return render(request, "index.html")
+    # jobListings = JobListing.objects.all()
 
-def JobListings(request):
-    return render(request, "index.html")
+    jobListings = [
+        {
+            "title": "Job Title aklsdjlf;sdkl;fsdkl;fjkl;saflk;skl;fskl;adfl;kasjf",
+            "companyName": "Company Name",
+            "description": "Description",
+            "location": "Location",
+            "applicationDeadline": "1/4/24",
+        }
+    ]
+
+    return render(request, "index.html", { "jobListings": jobListings })
--- jobBoard/settings.py
+++ jobBoard/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'app_jobBoard/static']
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
